3fc8b996d9f46a92371223d595268aed67e5b99f
JerryLiu0911 <89786918+JerryLiu0911@users.noreply.github.com>
2022-11-27 14:24:45 +0000

    Fixed GUI errors with camera and merged with GUI. Needs to start implement second screen

--- GUI.py
+++ GUI.py
@@ -1,35 +1,23 @@
-from kivy.clock import Clock
-from kivy.graphics import Line, Color, RoundedRectangle, Ellipse, Rectangle
+from kivy.graphics import Line, Color, RoundedRectangle
 from kivy.graphics.texture import Texture
 from kivy.uix.button import Button
 from kivymd.app import MDApp
+from kivymd.uix.button import MDFillRoundFlatButton
 from kivymd.uix.screen import Screen
 from kivy.core.window import Window
 from kivymd.uix.widget import MDWidget
 from kivy.uix.image import Image
 from kivymd.uix.fitimage import fitimage
-from kivy.core.camera import Camera
-import datetime
-import os
-from kivy.clock import mainthread
-from kivy.lang import Builder
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
-from kivy.resources import resource_add_path
-from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.camera import Camera
 from kivy.uix.label import Label
-from kivy.utils import platform
-from kivy_garden.xcamera import XCamera
 import cv2
 from kivymd.uix.fitimage import fitimage
 
 
-class camera(Camera):
-    pass
+class CropBox(MDWidget):
 
-
-class GUI(Screen):
     def __init__(self, **kwargs):
-        super(GUI, self).__init__(**kwargs)
+        super(CropBox, self).__init__(**kwargs)
         self.TouchDirectionY = None
         self.TouchDirectionX = None
         self.touch_x = None
@@ -37,24 +25,18 @@ class GUI(Screen):
         self.current_x = None
         self.current_y = None
         self.isTouchInBox = False
-        self.s = 70
         self.cropBoxHeight = self.height * 1
         self.cropBoxWidth = self.width * 1
-        with self.canvas.after:
-            Color(rgba=(1, 1, 1, 0.4))
+        with self.canvas:
+            Color(rgba=(0, 0, 0, 0.4))
             self.cropBox = RoundedRectangle(
                 pos=(self.center_x - self.cropBoxWidth / 2, self.center_y + self.height * 0.1),
                 size=(self.cropBoxWidth, self.cropBoxHeight),
                 width=2,
             )
-            self.Title = Label(
-                text="CamCalc",
-                # font_name=  'button',
-                color=(1, 1, 1, 1)
-            )
             self.cropPrompt = Label(
                 text="Align your math equation within the box",
-                color=(1, 1, 1),
+                color=(0, 0, 0),
                 halign='center',
                 pos=(self.center_x, self.cropBox.pos[1] + self.cropBoxHeight - self.height * 0.05),
                 font_size=12
@@ -67,7 +49,6 @@ class GUI(Screen):
         self.cropBox.size = self.cropBoxWidth, self.cropBoxHeight
         self.cropPrompt.pos = self.center_x - 50, self.cropBox.pos[1] + self.cropBoxHeight - self.height * 0.05
         self.cropPrompt.halign = 'center'
-        self.Title.pos = 10, self.height - self.height * 0.13
         print(self.cropPrompt.size)
 
     def on_touch_down(self, touch):
@@ -85,8 +66,7 @@ class GUI(Screen):
                 self.touch_y - self.cropBox.pos[1] - self.cropBoxHeight / 2)
             print(self.TouchDirectionX)
         else:
-            button = self.ids['button']
-            button.on_release = self.capture()
+            # button = self.ids['button']
             self.isTouchInBox = False
             print("someone's doing sum shit")
 
@@ -102,19 +82,38 @@ class GUI(Screen):
             self.current_y = touch.pos[1]
             print("dragging")
 
+
+class GUI(Screen):
+    def __init__(self, **kwargs):
+        super(GUI, self).__init__(**kwargs)
+        self.s = 70
+        self.camera = Camera(resolution=self.size, size=self.size, allow_stretch=True, play=True, index=0)
+        with self.canvas.after:
+            self.Title = Label(
+                text="CamCalc",
+                # font_name=  'button',
+                color=(1, 1, 1, 1)
+            )
+
     def crop(self):
         pass
 
+    def on_size(self, *args):
+        self.Title.pos = 10, self.height - self.height * 0.13
+        self.camera = Camera(resolution=self.size, size=self.size, allow_stretch=True, play=True, index=0)
+        self.add_widget(self.camera)
+        self.add_widget(CropBox())
+        self.add_widget(MDFillRoundFlatButton(size=(70, 70), pos=(self.center_x - 35, self.y + 50),
+                                              on_release=lambda x: self.capture())
+                        )
+
     def capture(self):
-        camera = self.ids['cam']
-        camera.export_to_png("input.jpg")
+        self.camera.export_to_png("input.jpg")
         print("capture")
 
 
 class CamCalc(MDApp):
     pass
-    # def build(self):
-    # Window.size = (350,650)
 
 
 CamCalc().run()
